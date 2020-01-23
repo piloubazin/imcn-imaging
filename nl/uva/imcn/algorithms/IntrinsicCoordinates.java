d56f2f402776f254244a633520bc83830f8a0890
piloubazin <piloubazin@gmail.com>
2020-01-23 09:58:27 +0100

    pca options

--- nl/uva/imcn/algorithms/IntrinsicCoordinates.java
+++ nl/uva/imcn/algorithms/IntrinsicCoordinates.java
@@ -53,76 +53,85 @@ public class IntrinsicCoordinates {
 	public final void setResolutions(float[] res) { rx=res[0]; ry=res[1]; rz=res[2]; }
 				
 	// create outputs
-	public final float[] geCoordinateImage() { return coordImage; }
+	public final float[] getCoordinateImage() { return coordImage; }
 	public final int[] getTransformedImage() { return transImage; }
 
 	public void execute(){
-		
+	    if (systemParam.equals("centroid_pca")) centroidPCA();
+	    else if (systemParam.equals("weighted_pca")) weightedPCA();
+	    else voxelPCA();
+	}
+	
+	private final void centroidPCA() { 
 	    // 1. build centroid list
 	    int nlb = ObjectLabeling.countLabels(labelImage, nx, ny, nz);
-	    int[] lbl = ObjectLabeling.listLabels(labelImage, nx, ny, nz);
-	    double[][] centroids = new double[nlb+1][4];
+	    int[] lbl = ObjectLabeling.listOrderedLabels(labelImage, nx, ny, nz);
+	    System.out.println("labels: "+nlb);
+		
+	    double[][] centroids = new double[nlb][4];
 	    for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
             int xyz = x+nx*y+nx*ny*z;
-            if (labelImage[xyz]>0) {
-                for (int lb=0;lb<nlb;lb++) if (labelImage[xyz]==lbl[lb]) {
-                    centroids[lb][X] += x;
-                    centroids[lb][Y] += y;
-                    centroids[lb][Z] += z;
-                    centroids[lb][T] += 1;
-                    lb = nlb;
-                }
+            for (int lb=1;lb<nlb;lb++) if (labelImage[xyz]==lbl[lb]) {
+                centroids[lb][X] += x;
+                centroids[lb][Y] += y;
+                centroids[lb][Z] += z;
+                centroids[lb][T] += 1;
+                lb = nlb;
             }
 	    }
-        for (int lb=0;lb<nlb;lb++) {
+        for (int lb=1;lb<nlb;lb++) {
             centroids[lb][X] /= centroids[lb][T];
             centroids[lb][Y] /= centroids[lb][T];
             centroids[lb][Z] /= centroids[lb][T];
         }
-        for (int lb=0;lb<nlb;lb++) {
-            centroids[nlb][X] += centroids[lb][X]/nlb;
-            centroids[nlb][Y] += centroids[lb][Y]/nlb;
-            centroids[nlb][Z] += centroids[lb][Z]/nlb;
+        for (int lb=1;lb<nlb;lb++) {
+            centroids[0][X] += centroids[lb][X]/nlb;
+            centroids[0][Y] += centroids[lb][Y]/nlb;
+            centroids[0][Z] += centroids[lb][Z]/nlb;
         }
-	    
+	    System.out.println("centroids:");
+		for (int lb=0;lb<nlb;lb++) {
+            System.out.println(lbl[lb]+" : ("+centroids[lb][X]+", "+centroids[lb][Y]+", "+centroids[lb][Z]+")");
+        }
+        
 	    // 2. shape PCA on the centroids
-		double[][] shape = new double[3][nlb];
-	    for (int lb=0;lb<nlb;lb++) {
-	        shape[X][lb] = centroids[lb][X]-centroids[nlb][X];
-	        shape[Y][lb] = centroids[lb][Y]-centroids[nlb][Y];
-	        shape[Z][lb] = centroids[lb][Z]-centroids[nlb][Z];
+		double[][] shape = new double[3][nlb-1];
+	    for (int lb=1;lb<nlb;lb++) {
+	        shape[X][lb-1] = centroids[lb][X]-centroids[0][X];
+	        shape[Y][lb-1] = centroids[lb][Y]-centroids[0][Y];
+	        shape[Z][lb-1] = centroids[lb][Z]-centroids[0][Z];
 	    }
 		Matrix M = new Matrix(shape);
 		SingularValueDecomposition svd = M.svd();
 		Matrix U = svd.getU();
 		
-		System.out.println("eigenvalues:");
-		double[] eigenval = new double[3];
+		System.out.println("singular values:");
+		double[] singval = new double[3];
 		for (int n=0;n<3;n++) {
-			eigenval[n] = svd.getSingularValues()[n]*svd.getSingularValues()[n];
-			System.out.print(eigenval[n]+", ");
+			singval[n] = svd.getSingularValues()[n];
+			System.out.print(singval[n]+", ");
 		}
 		double[][] axes = new double[3][3];
 		for (int n=0;n<3;n++) {
-		    axes[X][n] = eigenval[n]*U.get(X,n);
-		    axes[Y][n] = eigenval[n]*U.get(Y,n);
-		    axes[Z][n] = eigenval[n]*U.get(Z,n);
+		    axes[X][n] = singval[n]*U.get(X,n);
+		    axes[Y][n] = singval[n]*U.get(Y,n);
+		    axes[Z][n] = singval[n]*U.get(Z,n);
 		}
 		// build the corresponding coordinate system
 		coordImage = new float[3*nxyz];
 		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
 		    int xyz = x+nx*y+nx*ny*z;
-            coordImage[xyz+X*nxyz] = (float)( (x-centroids[nlb][X])*axes[X][X] 
-                                            + (y-centroids[nlb][Y])*axes[Y][X] 
-                                            + (z-centroids[nlb][Z])*axes[Z][X] ); 
+            coordImage[xyz+X*nxyz] = (float)( (x-centroids[0][X])*axes[X][X] 
+                                            + (y-centroids[0][Y])*axes[Y][X] 
+                                            + (z-centroids[0][Z])*axes[Z][X] ); 
                               
-            coordImage[xyz+Y*nxyz] = (float)( (x-centroids[nlb][X])*axes[X][Y] 
-                                            + (y-centroids[nlb][Y])*axes[Y][Y] 
-                                            + (z-centroids[nlb][Z])*axes[Z][Y] ); 
+            coordImage[xyz+Y*nxyz] = (float)( (x-centroids[0][X])*axes[X][Y] 
+                                            + (y-centroids[0][Y])*axes[Y][Y] 
+                                            + (z-centroids[0][Z])*axes[Z][Y] ); 
 
-            coordImage[xyz+Z*nxyz] = (float)( (x-centroids[nlb][X])*axes[X][Z] 
-                                            + (y-centroids[nlb][Y])*axes[Y][Z] 
-                                            + (z-centroids[nlb][Z])*axes[Z][Z] ); 
+            coordImage[xyz+Z*nxyz] = (float)( (x-centroids[0][X])*axes[X][Z] 
+                                            + (y-centroids[0][Y])*axes[Y][Z] 
+                                            + (z-centroids[0][Z])*axes[Z][Z] ); 
 
 		}
 		// map the data onto it
@@ -130,13 +139,214 @@ public class IntrinsicCoordinates {
 		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
 		    int xyz = x+nx*y+nx*ny*z;
 		    
-		    float xc = 3.0f*(x-nx/2.0f)/nx;
-		    float yc = 3.0f*(y-ny/2.0f)/ny;
-		    float zc = 3.0f*(z-nz/2.0f)/nz;
+		    float xc = 2.0f*(x-nx/2.0f)/nx;
+		    float yc = 2.0f*(y-ny/2.0f)/ny;
+		    float zc = 2.0f*(z-nz/2.0f)/nz;
 		    
-		    float xi = (float)(xc*axes[X][X] + yc*axes[X][Y] + zc*axes[X][Z] + centroids[nlb][X]);
-		    float yi = (float)(xc*axes[Y][X] + yc*axes[Y][Y] + zc*axes[Y][Z] + centroids[nlb][Y]);
-		    float zi = (float)(xc*axes[Z][X] + yc*axes[Z][Y] + zc*axes[Z][Z] + centroids[nlb][Z]);
+		    float xi = (float)(xc*axes[X][X] + yc*axes[X][Y] + zc*axes[X][Z] + centroids[0][X]);
+		    float yi = (float)(xc*axes[Y][X] + yc*axes[Y][Y] + zc*axes[Y][Z] + centroids[0][Y]);
+		    float zi = (float)(xc*axes[Z][X] + yc*axes[Z][Y] + zc*axes[Z][Z] + centroids[0][Z]);
+		    
+		    transImage[xyz] = ImageInterpolation.nearestNeighborClosestInterpolation(labelImage, xi,yi,zi, nx,ny,nz);
+		}
+	}
+
+	private final void weightedPCA() { 
+	    // 1. estimate volumes for weighting
+	    int nlb = ObjectLabeling.countLabels(labelImage, nx, ny, nz);
+	    int[] lbl = ObjectLabeling.listOrderedLabels(labelImage, nx, ny, nz);
+	    System.out.println("labels: "+nlb);
+		
+	    double[] weight = new double[nlb];
+	    for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                for (int lb=1;lb<nlb;lb++) if (labelImage[xyz]==lbl[lb]) {
+                    weight[lb]++;
+                    lb = nlb;
+                }
+            }
+	    }
+        for (int lb=1;lb<nlb;lb++) {
+            weight[lb] = 1.0/weight[lb]/(nlb-1.0);
+        }
+        
+        // 2. build covariance matrix
+        double[] avg = new double[3];
+        for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                for (int lb=1;lb<nlb;lb++) if (labelImage[xyz]==lbl[lb]) {
+                    avg[X] += weight[lb]*x;
+                    avg[Y] += weight[lb]*y;
+                    avg[Z] += weight[lb]*z;
+                    lb = nlb;
+                }
+            }
+        }
+        
+	    double[][] covar = new double[3][3];
+	    for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                for (int lb=1;lb<nlb;lb++) if (labelImage[xyz]==lbl[lb]) {
+                    covar[X][X] += weight[lb]*(x-avg[X])*(x-avg[X]);
+                    covar[X][Y] += weight[lb]*(x-avg[X])*(y-avg[Y]);
+                    covar[X][Z] += weight[lb]*(x-avg[X])*(z-avg[Z]);
+
+                    covar[Y][X] += weight[lb]*(y-avg[Y])*(x-avg[X]);
+                    covar[Y][Y] += weight[lb]*(y-avg[Y])*(y-avg[Y]);
+                    covar[Y][Z] += weight[lb]*(y-avg[Y])*(z-avg[Z]);
+
+                    covar[Z][X] += weight[lb]*(z-avg[Z])*(x-avg[X]);
+                    covar[Z][Y] += weight[lb]*(z-avg[Z])*(y-avg[Y]);
+                    covar[Z][Z] += weight[lb]*(z-avg[Z])*(z-avg[Z]);
+                    lb = nlb;
+                }
+            }
+        }
+                    
+	    // 3. shape PCA on the covarianca
+		Matrix M = new Matrix(covar);
+		SingularValueDecomposition svd = M.svd();
+		Matrix U = svd.getU();
+		
+		System.out.println("singular values:");
+		double[] singval = new double[3];
+		for (int n=0;n<3;n++) {
+			singval[n] = FastMath.sqrt(svd.getSingularValues()[n]);
+			System.out.print(singval[n]+", ");
+		}
+		double[][] axes = new double[3][3];
+		for (int n=0;n<3;n++) {
+		    axes[X][n] = singval[n]*U.get(X,n);
+		    axes[Y][n] = singval[n]*U.get(Y,n);
+		    axes[Z][n] = singval[n]*U.get(Z,n);
+		}
+		// build the corresponding coordinate system
+		coordImage = new float[3*nxyz];
+		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+		    int xyz = x+nx*y+nx*ny*z;
+            coordImage[xyz+X*nxyz] = (float)( (x-avg[X])*axes[X][X] 
+                                            + (y-avg[Y])*axes[Y][X] 
+                                            + (z-avg[Z])*axes[Z][X] ); 
+                              
+            coordImage[xyz+Y*nxyz] = (float)( (x-avg[X])*axes[X][Y] 
+                                            + (y-avg[Y])*axes[Y][Y] 
+                                            + (z-avg[Z])*axes[Z][Y] ); 
+
+            coordImage[xyz+Z*nxyz] = (float)( (x-avg[X])*axes[X][Z] 
+                                            + (y-avg[Y])*axes[Y][Z] 
+                                            + (z-avg[Z])*axes[Z][Z] ); 
+
+		}
+		// map the data onto it
+		transImage = new int[nxyz];
+		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+		    int xyz = x+nx*y+nx*ny*z;
+		    
+		    float xc = 2.0f*(x-nx/2.0f)/nx;
+		    float yc = 2.0f*(y-ny/2.0f)/ny;
+		    float zc = 2.0f*(z-nz/2.0f)/nz;
+		    
+		    float xi = (float)(xc*axes[X][X] + yc*axes[X][Y] + zc*axes[X][Z] + avg[X]);
+		    float yi = (float)(xc*axes[Y][X] + yc*axes[Y][Y] + zc*axes[Y][Z] + avg[Y]);
+		    float zi = (float)(xc*axes[Z][X] + yc*axes[Z][Y] + zc*axes[Z][Z] + avg[Z]);
+		    
+		    transImage[xyz] = ImageInterpolation.nearestNeighborClosestInterpolation(labelImage, xi,yi,zi, nx,ny,nz);
+		}
+	}
+
+	private final void voxelPCA() { 
+	    // 1. estimate volumes for weighting
+	    int nlb = ObjectLabeling.countLabels(labelImage, nx, ny, nz);
+	    int[] lbl = ObjectLabeling.listOrderedLabels(labelImage, nx, ny, nz);
+	    System.out.println("labels: "+nlb);
+		
+	    double weight = 0.0;
+	    for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                weight++;
+            }
+	    }
+        weight = 1.0/weight;
+        
+        // 2. build covariance matrix
+        double[] avg = new double[3];
+        for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                avg[X] += weight*x;
+                avg[Y] += weight*y;
+                avg[Z] += weight*z;
+            }
+        }
+        
+	    double[][] covar = new double[3][3];
+	    for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+            int xyz = x+nx*y+nx*ny*z;
+            if (labelImage[xyz]>lbl[0]) {
+                covar[X][X] += weight*(x-avg[X])*(x-avg[X]);
+                covar[X][Y] += weight*(x-avg[X])*(y-avg[Y]);
+                covar[X][Z] += weight*(x-avg[X])*(z-avg[Z]);
+
+                covar[Y][X] += weight*(y-avg[Y])*(x-avg[X]);
+                covar[Y][Y] += weight*(y-avg[Y])*(y-avg[Y]);
+                covar[Y][Z] += weight*(y-avg[Y])*(z-avg[Z]);
+
+                covar[Z][X] += weight*(z-avg[Z])*(x-avg[X]);
+                covar[Z][Y] += weight*(z-avg[Z])*(y-avg[Y]);
+                covar[Z][Z] += weight*(z-avg[Z])*(z-avg[Z]);
+            }
+        }
+                    
+	    // 3. shape PCA on the covarianca
+		Matrix M = new Matrix(covar);
+		SingularValueDecomposition svd = M.svd();
+		Matrix U = svd.getU();
+		
+		System.out.println("singular values:");
+		double[] singval = new double[3];
+		for (int n=0;n<3;n++) {
+			singval[n] = FastMath.sqrt(svd.getSingularValues()[n]);
+			System.out.print(singval[n]+", ");
+		}
+		double[][] axes = new double[3][3];
+		for (int n=0;n<3;n++) {
+		    axes[X][n] = singval[n]*U.get(X,n);
+		    axes[Y][n] = singval[n]*U.get(Y,n);
+		    axes[Z][n] = singval[n]*U.get(Z,n);
+		}
+		// build the corresponding coordinate system
+		coordImage = new float[3*nxyz];
+		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+		    int xyz = x+nx*y+nx*ny*z;
+            coordImage[xyz+X*nxyz] = (float)( (x-avg[X])*axes[X][X] 
+                                            + (y-avg[Y])*axes[Y][X] 
+                                            + (z-avg[Z])*axes[Z][X] ); 
+                              
+            coordImage[xyz+Y*nxyz] = (float)( (x-avg[X])*axes[X][Y] 
+                                            + (y-avg[Y])*axes[Y][Y] 
+                                            + (z-avg[Z])*axes[Z][Y] ); 
+
+            coordImage[xyz+Z*nxyz] = (float)( (x-avg[X])*axes[X][Z] 
+                                            + (y-avg[Y])*axes[Y][Z] 
+                                            + (z-avg[Z])*axes[Z][Z] ); 
+
+		}
+		// map the data onto it
+		transImage = new int[nxyz];
+		for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) for (int z=0;z<nz;z++) {
+		    int xyz = x+nx*y+nx*ny*z;
+		    
+		    float xc = 2.0f*(x-nx/2.0f)/nx;
+		    float yc = 2.0f*(y-ny/2.0f)/ny;
+		    float zc = 2.0f*(z-nz/2.0f)/nz;
+		    
+		    float xi = (float)(xc*axes[X][X] + yc*axes[X][Y] + zc*axes[X][Z] + avg[X]);
+		    float yi = (float)(xc*axes[Y][X] + yc*axes[Y][Y] + zc*axes[Y][Z] + avg[Y]);
+		    float zi = (float)(xc*axes[Z][X] + yc*axes[Z][Y] + zc*axes[Z][Z] + avg[Z]);
 		    
 		    transImage[xyz] = ImageInterpolation.nearestNeighborClosestInterpolation(labelImage, xi,yi,zi, nx,ny,nz);
 		}
--- nl/uva/imcn/methods/BasicSom.java
+++ nl/uva/imcn/methods/BasicSom.java
@@ -118,6 +118,20 @@ public class BasicSom {
 	    return map;
 	}
 	
+	public final float[][] mapSomOnData3D() {
+	    float[][] map = new float[ndata][3];
+	    for (int n=0;n<ndata;n++) if (mask[n]) {
+	        int node = findClosestNode(data[n]);
+            int z0 = Numerics.floor(node/(nx*ny));
+            int y0 = Numerics.floor((node-nx*ny*z0)/nx);
+            int x0 = node - z0*nx*ny - y0*nx;
+	        map[n][0] = x0/(nx-1.0f);
+	        map[n][1] = y0/(ny-1.0f);
+	        map[n][2] = z0/(nz-1.0f);
+	    }
+	    return map;
+	}
+	
 	/**
 	 *   main routine
 	 */
@@ -149,6 +163,47 @@ public class BasicSom {
 	        if (sample) {        
                 int node = findClosestNode(data[idx]);
                 float diff = updateNodeNeighborhood2D(t, node, data[idx]); 
+                System.out.println("iteration "+t+" (alpha: "+kernelAlpha+", ngb: "+kernelSize+"): "+diff);
+            } else {
+                // skip and resample
+                t--;
+            }
+	    }
+	    
+	    return;
+	}
+	
+	/**
+	 *   main routine
+	 */
+	final public void run_som3D() {
+	    
+	    // 1. random weight initialization
+	    //initializeRandomSom();
+	    
+	    // or PCA-based
+	    initializePcaSom3D();
+	    
+	    // 2. main loop: sample, update
+	    for (int t=0;t<iter;t++) {
+	        // randomly pick a value
+	        int idx = Numerics.floor(ndata*FastMath.random());
+	        if (idx==ndata) idx=ndata-1;
+	        
+	        boolean sample = false;
+	        if (mask[idx]) {
+	            if (proba!=null) {
+	                // only sample if above proba
+	                if (proba[idx]>FastMath.random()) {
+	                    sample = true;
+	                }
+	            } else {
+	                sample = true;
+	            }
+	        }
+	        if (sample) {        
+                int node = findClosestNode(data[idx]);
+                float diff = updateNodeNeighborhood3D(t, node, data[idx]); 
                 System.out.println("iteration "+t+" (alpha: "+kernelAlpha+", ngb: "+kernelSize+"): "+diff);
             } else {
                 // skip and resample
@@ -261,6 +316,88 @@ public class BasicSom {
         }
 	}
    
+	/**
+	 *  PCA initialization
+	 *   from the data values themselves
+	 */
+	final public void initializePcaSom3D() {
+	    float[] avg = new float[dim];
+	    for (int d=0;d<dim;d++) {
+	       avg[d] = 0.0f;
+	    }
+	    double nsample = 0;
+	    if (proba!=null) {
+            for (int n=0;n<ndata;n++) if (mask[n]) {
+                for (int d=0;d<dim;d++) {
+                    avg[d] += proba[n]*data[n][d];
+                }
+                nsample+=proba[n];
+            }
+	    } else {
+            for (int n=0;n<ndata;n++) if (mask[n]) {
+                for (int d=0;d<dim;d++) {
+                    avg[d] += data[n][d];
+                }
+                nsample++;
+            }
+        }
+        for (int d=0;d<dim;d++) {
+            avg[d] /= (float)nsample;
+        }
+        
+        double[][] covar = new double[dim][dim];
+	    if (proba!=null) {
+            for (int n=0;n<ndata;n++) if (mask[n]) {
+                for (int i=0;i<dim;i++) {
+                    for (int j=i;j<dim;j++) {
+                        covar[i][j] += proba[n]*(data[n][i]-avg[i])*(data[n][j]-avg[j])/nsample;
+                    }
+                }
+            }
+	    } else {
+            for (int n=0;n<ndata;n++) if (mask[n]) {
+                for (int i=0;i<dim;i++) {
+                    for (int j=i;j<dim;j++) {
+                        covar[i][j] += (data[n][i]-avg[i])*(data[n][j]-avg[j])/nsample;
+                    }
+                }
+            }
+        }
+	    for (int i=0;i<dim;i++) {
+	        for (int j=0;j<i;j++) {
+	            covar[i][j] = covar[j][i];
+	        }
+	    }
+	    Matrix M = new Matrix(covar);
+        SingularValueDecomposition svd = M.svd();
+                    
+        // keep the first two for 2D SOM
+        float[] eig1 = new float[dim];
+        float[] eig2 = new float[dim];
+        double sig1 = FastMath.sqrt(svd.getSingularValues()[0]);
+        double sig2 = FastMath.sqrt(svd.getSingularValues()[1]);
+        
+        for (int i=0;i<dim;i++) {
+            eig1[i] = (float)(sig1*svd.getV().get(i,0));                        
+            eig2[i] = (float)(sig2*svd.getV().get(i,1));
+        }
+	    // map the avg +/- eigenvalues to som dimensions
+        for (int x=0;x<nx;x++) {
+            float dx = 2.0f*(x-(nx-1.0f)/2.0f)/((nx-1.0f)/2.0f);
+            for (int y=0;y<ny;y++) {
+                float dy = 2.0f*(y-(ny-1.0f)/2.0f)/((ny-1.0f)/2.0f);
+                for (int z=0;z<nz;z++) {
+                    float dz = 2.0f*(z-(nz-1.0f)/2.0f)/((nz-1.0f)/2.0f);
+                        if (lattice[x+nx*y+nx*ny*z]) {
+                        for (int d=0;d<dim;d++) {
+                            som[d][x+nx*y+nx*ny*z] = avg[d] + dx*eig1[d] + dy*eig2[d];
+                        }
+                    }
+                }
+            }
+        }
+	}
+   
     /** 
 	 *  find closest point to data
 	 */
@@ -307,6 +444,44 @@ public class BasicSom {
             for (int d=0;d<dim;d++) {
                 float delta = weight*(val[d]-som[d][x+nx*y]);
                 som[d][x+nx*y] += delta;
+                diff += delta*delta;
+            }
+            if (diff>maxdiff) maxdiff = diff;
+        }
+
+        return maxdiff;
+    }
+    
+    /**
+     *  update within a neighborhood (specific to SOM shape)
+     */
+    final public float updateNodeNeighborhood3D(int t, int node, float[] val) {
+        // get the coordinates in the SOM
+        int z0 = Numerics.floor(node/(nx*ny));
+        int y0 = Numerics.floor((node-z0*nx*ny)/nx);
+        int x0 = node - z0*nx*ny - y0*nx;
+        
+        // update the kernel functions
+        kernelUpdate(t);
+        
+        // find value boundaries
+        int xmin = Numerics.max(0, Numerics.floor(x0-kernelSize));
+        int xmax = Numerics.min(nx, Numerics.ceil(x0+kernelSize+1));
+        int ymin = Numerics.max(0, Numerics.floor(y0-kernelSize));
+        int ymax = Numerics.min(ny, Numerics.ceil(y0+kernelSize+1));
+        int zmin = Numerics.max(0, Numerics.floor(z0-kernelSize));
+        int zmax = Numerics.min(nz, Numerics.ceil(z0+kernelSize+1));
+        
+        // compute the update
+        float maxdiff = 0.0f;
+        for (int x=xmin;x<xmax;x++) for (int y=ymin;y<ymax;y++) for (int z=zmin;z<zmax;z++) if (lattice[x+nx*y+nx*ny*z]) {
+            float dist = (x-x0)*(x-x0)+(y-y0)*(y-y0)+(z-z0)*(z-z0);   
+            
+            float weight = kernelWeight(dist);
+            float diff = 0.0f;
+            for (int d=0;d<dim;d++) {
+                float delta = weight*(val[d]-som[d][x+nx*y+nx*ny*z]);
+                som[d][x+nx*y+nx*ny*z] += delta;
                 diff += delta*delta;
             }
             if (diff>maxdiff) maxdiff = diff;
