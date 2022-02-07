70bc8c7dbf71d03e5ddbb118a60dd22a8cf26093
Pierre-Louis Bazin <piloubazin@gmail.com>
2022-02-07 20:39:36 +0100

    new module for rescaling lines

---
+++ nl/uva/imcn/algorithms/LinearFiberScaling.java
@@ -0,0 +1,109 @@
+package nl.uva.imcn.algorithms;
+
+import nl.uva.imcn.utilities.Ngb;
+import nl.uva.imcn.utilities.Numerics;
+import nl.uva.imcn.utilities.BasicInfo;
+import nl.uva.imcn.libraries.ImageFilters;
+import nl.uva.imcn.libraries.ImageStatistics;
+import nl.uva.imcn.libraries.ObjectExtraction;
+import nl.uva.imcn.libraries.ObjectLabeling;
+import nl.uva.imcn.structures.BinaryHeapPair;
+
+import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.stat.descriptive.rank.Percentile;
+import org.apache.commons.math3.special.Erf;
+//import org.apache.commons.math3.analysis.*;
+
+import java.util.BitSet;
+
+public class LinearFiberScaling {
+	
+	// jist containers
+	private float[] probaImage;
+	private float[] thetaImage;
+	private float[] lengthImage;
+	
+	private int scaling = 7;
+	private int kept = 5;
+	private float detectionThreshold = 1e-9f;
+		
+	// global variables
+	private int nx, ny, nz, nxyz;
+	private float rx, ry, rz;
+	
+	private static final boolean debug=true;
+	
+	//set inputs
+	public final void setProbaImage(float[] val) { probaImage = val; }
+	public final void setThetaImage(float[] val) { thetaImage = val; }
+	public final void setLengthImage(float[] val) { lengthImage = val; }
+
+	public final void setScaling(int val) { scaling = val; }
+	public final void setNumberKept(int val) { kept = val; }
+	public final void setDetectionThreshold(float val) { detectionThreshold = val; }
+		
+	// set generic inputs	
+	public final void setDimensions(int x, int y, int z) { nx=x; ny=y; nz=z; nxyz=nx*ny*nz; }
+	public final void setDimensions(int[] dim) { nx=dim[0]; ny=dim[1]; nz=dim[2]; nxyz=nx*ny*nz; }
+	public final void setResolutions(float x, float y, float z) { rx=x; ry=y; rz=z; }
+	public final void setResolutions(float[] res) { rx=res[0]; ry=res[1]; rz=res[2]; }
+			
+	// create outputs
+	public final float[] getScaledProbabilityImage() { return probaImage;}
+	public final float[] getScaledLengthImage() { return lengthImage;}
+	public final float[] getScaledAngleImage() { return thetaImage;}
+	
+	public void execute(){
+		BasicInfo.displayMessage("linear fiber scaling:\n");
+		
+		// import the inputImage data into 1D arrays: already done
+		int mx = Numerics.ceil((float)nx/scaling);
+		int my = Numerics.ceil((float)ny/scaling);
+		
+		BasicInfo.displayMessage("...scale data from "+nx+"x"+ny+" to "+mx+"x"+my);
+		
+		float[] probasc = new float[mx*my*nz*kept];
+		float[] thetasc = new float[mx*my*nz*kept];
+		float[] lengthsc = new float[mx*my*nz*kept];
+		for (int x=0;x<mx;x++) for (int y=0;y<my;y++) for (int z=0;z<nz;z++) {
+		    // find the largest groups
+		    int idm = x+mx*y+mx*my*z;
+		    for (int k=0;k<kept;k++) {
+		        // first locate the highest probability
+		        float maxproba = 0.0f;
+		        for (int dx=0;dx<scaling;dx++) for (int dy=0;dy<scaling;dy++) {
+		            if (x*scaling+dx<nx && y*scaling+dy<ny) {
+		                int idn = x*scaling+dx + nx*(y*scaling+dy) + nx*ny*z;
+		                if (probaImage[idn]>maxproba) maxproba = probaImage[idn];
+		            }
+		        }
+		        // then group the corresponding voxels
+		        if (maxproba>0) {
+                    for (int dx=0;dx<scaling;dx++) for (int dy=0;dy<scaling;dy++) {
+                        if (x*scaling+dx<nx && y*scaling+dy<ny) {
+                            int idn = x*scaling+dx + nx*(y*scaling+dy) + nx*ny*z;
+                            if (probaImage[idn]>=maxproba-detectionThreshold) {
+                                probasc[idm+mx*my*nz*k] += probaImage[idn];
+                                thetasc[idm+mx*my*nz*k] += probaImage[idn]*thetaImage[idn];
+                                lengthsc[idm+mx*my*nz*k] += probaImage[idn]*lengthImage[idn];
+                                // reset probability once it has been used
+                                probaImage[idn] = 0.0f;
+                            }
+                        }
+                    }
+                    if (probasc[idm+mx*my*nz*k]>0) {
+                        thetasc[idm+mx*my*nz*k] /= probasc[idm+mx*my*nz*k];
+                        lengthsc[idm+mx*my*nz*k] /= probasc[idm+mx*my*nz*k];
+                        probasc[idm+mx*my*nz*k] /= scaling*scaling;
+                    }
+                }
+            }
+        }
+        probaImage = probasc;
+        thetaImage = thetasc;
+        lengthImage = lengthsc;
+		    
+		return;
+    }
+
+}
