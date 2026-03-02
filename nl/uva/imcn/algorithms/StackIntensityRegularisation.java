84b250a4535a8766785328e686b28e941712ed33
Pilou Bazin <piloubazin@gmail.com>
2026-03-02 11:50:46 +0100

    basic localized intensity normalization (for testing only)

--- nl/uva/imcn/algorithms/StackIntensityRegularisation.java
+++ nl/uva/imcn/algorithms/StackIntensityRegularisation.java
@@ -390,4 +390,367 @@ public class StackIntensityRegularisation {
 	    
 		System.out.print("Done\n");
 	}
+	
+	public void executeSubdivide(int sub) {
+	    
+	    // mask zero values or estimate background
+	    boolean[] mask = new boolean[nxyz];
+	    if (foreground==null) {
+            for (int xyz=0;xyz<nxyz;xyz++) 
+                if (image[xyz]!=0) mask[xyz] = true;
+                else mask[xyz] = false;
+        } else {
+            int nmask=0;
+            for (int xyz=0;xyz<nxyz;xyz++) {
+                if (foreground[xyz]>0.5) {
+                    mask[xyz] = true;
+                    nmask++;
+                }
+                else mask[xyz] = false;
+            }
+            System.out.print("mask size: "+nmask);
+        }     
+        
+        // remove outlier values (high) from computation?
+        if (rmax>0) {
+            for (int z=0;z<nz;z++) {
+                double[] intens = new double[nx*ny];
+                int ni = 0;
+                for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) {
+                    int xyz = x+nx*y+nx*ny*z;
+                    intens[ni] = image[xyz];
+                    ni++;
+                }
+                Percentile measure = new Percentile();
+                double imax = measure.evaluate(intens, 0, ni, rmax);
+                
+                for (int x=0;x<nx;x++) for (int y=0;y<ny;y++) {
+                    int xyz = x+nx*y+nx*ny*z;
+                    if (image[xyz]>imax) mask[xyz] = false;
+                }
+            }
+        }
+	    
+	    // per neighborhood??:
+	    double[] differences = new double [mem*nx*ny];
+	    int ndiff = 0;
+	    double minbias = 0;
+	    double maxbias = 0;
+	    double minfactor = 1;
+	    double maxfactor = 1;
+	    double minfit = 1;
+	    int minbiasid = -1;
+	    int maxbiasid = -1;
+	    int minfactorid = -1;
+	    int maxfactorid = -1;
+	    int minfitid = -1;
+	    
+	    int mid = Numerics.round(nz/2.0f);
+	    for (int z=mid+1;z<nz;z++) {
+	        System.out.print("Processing slice "+z);
+	        int sx = Numerics.floor(nx/sub);
+	        int sy = Numerics.floor(ny/sub);
+	        
+	        for (int dx=0;dx<sub;dx++) for (int dy=0;dy<sub;dy++) {
+	            
+	       
+                ndiff = 0;
+                for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                    int xyz = x+nx*y+nx*ny*z;
+                    int ngb1 = xyz-nx*ny;
+                    if (mask[xyz] && mask[ngb1]) {
+                        differences[ndiff] = image[xyz]-image[ngb1];
+                        ndiff++;
+                    }
+                    for (int m=1;m<mem;m++) {
+                        if (z>mid+m) {
+                            int ngb2 = xyz-(m+1)*nx*ny;
+                            if (mask[xyz] && mask[ngb2]) {
+                                differences[ndiff] = image[xyz]-image[ngb2];
+                                ndiff++;
+                            }
+                        }
+                    }
+                }
+                if (ndiff>0) {
+                    // find the distribution excluding edges: only use the 50% central differences
+                    Percentile measure = new Percentile();
+                    double min = measure.evaluate(differences, 0, ndiff, 50-cutoff/2);
+                    double max = measure.evaluate(differences, 0, ndiff, 50+cutoff/2);
+                    //double max = measure.evaluate(differences, 0, ndiff, cutoff);
+                
+                    // estimate the scaling factor (or curve)
+                    double[] curr = new double[ndiff];
+                    double[] prev = new double[ndiff];
+                    double mean = 0;
+                    int nkept=0;
+                    ndiff = 0;
+                    for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                        int xyz = x+nx*y+nx*ny*z;
+                        int ngb1 = xyz-nx*ny;
+                        if (mask[xyz] && mask[ngb1]) {
+                            if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                            //if (differences[ndiff]<=max) {
+                                curr[nkept] = image[xyz];
+                                prev[nkept] = image[ngb1];
+                                mean += image[xyz];
+                                nkept++;
+                            }
+                            ndiff++;
+                        }
+                        for (int m=1;m<mem;m++) {
+                            if (z>mid+m) {
+                                int ngb2 = xyz-(m+1)*nx*ny;
+                                if (mask[xyz] && mask[ngb2]) {
+                                    if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                    //if (differences[ndiff]<=max) {
+                                        curr[nkept] = image[xyz];
+                                        prev[nkept] = image[ngb2];
+                                        mean += image[xyz];
+                                        nkept++;
+                                    }
+                                    ndiff++;
+                                }
+                            }
+                        }
+                    }
+                    if (nkept>0) {
+                        mean /= (double)nkept;
+                            
+                        // linear least squares
+                        double[][] fit = new double[nkept][1];
+                        double[][] poly = new double[nkept][2];
+                        for (int n=0;n<nkept;n++) {
+                            fit[n][0] = curr[n];
+                            poly[n][0] = 1.0;
+                            poly[n][1] = prev[n];
+                        }
+                        // invert the linear model
+                        Matrix mtx = new Matrix(poly);
+                        Matrix smp = new Matrix(fit);
+                        Matrix val = mtx.solve(smp);
+                            
+                        // compute the new values and residuals
+                        double residual = 0;
+                        double variance = 0;
+                        nkept=0;
+                        ndiff = 0;
+                        for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                            int xyz = x+nx*y+nx*ny*z;
+                            if (mask[xyz]) {
+                                // replace the image values directly -> possible drift? (shouldn't be the case)
+                                int ngb1 = xyz-nx*ny;
+                                double expected1 = val.get(0,0) + image[ngb1]*val.get(1,0);
+                                if (mask[ngb1]) { 
+                                    if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                    //if (differences[ndiff]<=max) {
+                                        // compute residuals only where relevant
+                                        variance += (image[xyz]-mean)*(image[xyz]-mean);
+                                        residual += (image[xyz]-expected1)*(image[xyz]-expected1);
+                                        nkept++;
+                                    }
+                                    ndiff++;
+                                }
+                                for (int m=1;m<mem;m++) {
+                                    if (z>mid+m) {
+                                        int ngb2 = xyz-(m+1)*nx*ny;
+                                        double expected2 = val.get(0,0) + image[ngb2]*val.get(1,0);
+                                        if (mask[ngb2]) { 
+                                            if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                            //if (differences[ndiff]<=max) {
+                                                // compute residuals only where relevant
+                                                variance += (image[xyz]-mean)*(image[xyz]-mean);
+                                                residual += (image[xyz]-expected2)*(image[xyz]-expected2);
+                                                nkept++;
+                                            }
+                                            ndiff++;
+                                        }
+                                    }
+                                }
+                                // change values
+                                image[xyz] = (float)((image[xyz]-val.get(0,0))/val.get(1,0)); 
+                            } else if (image[xyz]>rmax) {
+                                // change values in masked intensity regions too
+                                image[xyz] = (float)((image[xyz]-val.get(0,0))/val.get(1,0)); 
+                            }
+                        }
+                        double rsquare = 1.0;
+                        if (variance>0) rsquare = Numerics.max(1.0 - (residual/variance), 0.0);
+                        System.out.print(" bias: "+val.get(0,0));
+                        System.out.println(" scaling: "+val.get(1,0));
+                        //System.out.println("residuals R^2: "+rsquare);
+                        if (val.get(0,0)>maxbias) { maxbias = val.get(0,0); maxbiasid = z; }
+                        if (val.get(0,0)<minbias) { minbias = val.get(0,0); minbiasid = z; }
+                        if (val.get(1,0)>maxfactor) { maxfactor = val.get(1,0); maxfactorid = z; }
+                        if (val.get(1,0)<minfactor) { minfactor = val.get(1,0); minfactorid = z; }
+                        if (rsquare<minfit) { minfit = rsquare; minfitid = z; }
+                    } else {
+                        System.out.println("no good data: skip");
+                    }
+                } else {
+                    System.out.println("empty mask overlap: skip");
+                }
+            }
+        }
+	    for (int z=mid-1;z>=0;z--) {
+	        System.out.print("Processing slice "+z);
+	        int sx = Numerics.floor(nx/sub);
+	        int sy = Numerics.floor(ny/sub);
+	        
+	        for (int dx=0;dx<sub;dx++) for (int dy=0;dy<sub;dy++) {
+                ndiff = 0;
+                for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                    int xyz = x+nx*y+nx*ny*z;
+                    int ngb1 = xyz+nx*ny;
+                    if (mask[xyz] && mask[ngb1]) {
+                        differences[ndiff] = image[xyz]-image[ngb1];
+                        ndiff++;
+                    }
+                    for (int m=1;m<mem;m++) {
+                        if (z<mid-m) {
+                            int ngb2 = xyz+(m+1)*nx*ny;
+                            if (mask[xyz] && mask[ngb2]) {
+                                differences[ndiff] = image[xyz]-image[ngb2];
+                                ndiff++;
+                            }
+                        }
+                    }
+                }
+                if (ndiff>0) {
+                    // find the distribution excluding edges: only use the 50% central differences
+                    Percentile measure = new Percentile();
+                    double min = measure.evaluate(differences, 0, ndiff, 50-cutoff/2);
+                    double max = measure.evaluate(differences, 0, ndiff, 50+cutoff/2);
+                    //double max = measure.evaluate(differences, 0, ndiff, cutoff);
+                
+                    // estimate the scaling factor (or curve)
+                    double[] curr = new double[ndiff];
+                    double[] prev = new double[ndiff];
+                    double mean = 0;
+                    int nkept=0;
+                    ndiff = 0;
+                    for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                        int xyz = x+nx*y+nx*ny*z;
+                        int ngb1 = xyz+nx*ny;
+                        if (mask[xyz] && mask[ngb1]) {
+                            if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                            //if (differences[ndiff]<=max) {
+                                curr[nkept] = image[xyz];
+                                prev[nkept] = image[ngb1];
+                                mean += image[xyz];
+                                nkept++;
+                            }
+                            ndiff++;
+                        }
+                        for (int m=1;m<mem;m++) {
+                            if (z<mid-m) {
+                                int ngb2 = xyz+(m+1)*nx*ny;
+                                if (mask[xyz] && mask[ngb2]) {
+                                    if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                    //if (differences[ndiff]<=max) {
+                                        curr[nkept] = image[xyz];
+                                        prev[nkept] = image[ngb2];
+                                        mean += image[xyz];
+                                        nkept++;
+                                    }
+                                    ndiff++;
+                                }
+                            }
+                        }
+                    }
+                    if (nkept>0) {
+                        mean /= (double)nkept;
+                            
+                        // linear least squares
+                        double[][] fit = new double[nkept][1];
+                        double[][] poly = new double[nkept][2];
+                        for (int n=0;n<nkept;n++) {
+                            fit[n][0] = curr[n];
+                            poly[n][0] = 1.0;
+                            poly[n][1] = prev[n];
+                        }
+                        // invert the linear model
+                        Matrix mtx = new Matrix(poly);
+                        Matrix smp = new Matrix(fit);
+                        Matrix val = mtx.solve(smp);
+                            
+                        // compute the new values and residuals
+                        double residual = 0;
+                        double variance = 0;
+                        nkept=0;
+                        ndiff = 0;
+                        for (int x=dx*sx;x<(dx+1)*sx;x++) for (int y=dy*sy;y<(dy+1)*sy;y++) {
+                            int xyz = x+nx*y+nx*ny*z;
+                            if (mask[xyz]) {
+                                // replace the image values directly -> possible drift? (shouldn't be the case)
+                                int ngb1 = xyz+nx*ny;
+                                double expected1 = val.get(0,0) + image[ngb1]*val.get(1,0);
+                                if (mask[ngb1]) { 
+                                    if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                    //if (differences[ndiff]<=max) {
+                                        // compute residuals only where relevant
+                                        variance += (image[xyz]-mean)*(image[xyz]-mean);
+                                        residual += (image[xyz]-expected1)*(image[xyz]-expected1);
+                                        nkept++;
+                                    }
+                                    ndiff++;
+                                }
+                                for (int m=1;m<mem;m++) {
+                                    if (z<mid-m) {
+                                        int ngb2 = xyz+(m+1)*nx*ny;
+                                        double expected2 = val.get(0,0) + image[ngb2]*val.get(1,0);
+                                        if (mask[ngb2]) { 
+                                            if (differences[ndiff]>=min && differences[ndiff]<=max) {
+                                            //if (differences[ndiff]<=max) {
+                                                // compute residuals only where relevant
+                                                variance += (image[xyz]-mean)*(image[xyz]-mean);
+                                                residual += (image[xyz]-expected2)*(image[xyz]-expected2);
+                                                nkept++;
+                                            }
+                                            ndiff++;
+                                        }
+                                    }
+                                }
+                                // change values
+                                image[xyz] = (float)((image[xyz]-val.get(0,0))/val.get(1,0)); 
+                            } else if (image[xyz]>rmax) {
+                                // change values in masked intensity regions too
+                                image[xyz] = (float)((image[xyz]-val.get(0,0))/val.get(1,0)); 
+                            }
+                        }
+                        double rsquare = 1.0;
+                        if (variance>0) rsquare = Numerics.max(1.0 - (residual/variance), 0.0);
+                        System.out.print(" bias: "+val.get(0,0));
+                        System.out.println(" scaling: "+val.get(1,0));
+                        //System.out.println("residuals R^2: "+rsquare);
+                        if (val.get(0,0)>maxbias) { maxbias = val.get(0,0); maxbiasid = z; }
+                        if (val.get(0,0)<minbias) { minbias = val.get(0,0); minbiasid = z; }
+                        if (val.get(1,0)>maxfactor) { maxfactor = val.get(1,0); maxfactorid = z; }
+                        if (val.get(1,0)<minfactor) { minfactor = val.get(1,0); minfactorid = z; }
+                        if (rsquare<minfit) { minfit = rsquare; minfitid = z; }
+                    } else {
+                        System.out.println("no good data: skip");
+                    }
+                } else {
+                    System.out.println("empty mask overlap: skip");
+                }
+            }
+        }
+        System.out.println("bias: ["+minbias+" ("+minbiasid+"), "+maxbias+" ("+maxbiasid+"]");
+        System.out.println("scaling: ["+minfactor+" ("+minfactorid+"), "+maxfactor+" ("+maxfactorid+"]");
+        System.out.println("min residuals R^2: "+minfit+" ("+minfitid+")");
+	    // provide a global stabilisation? e.g. do the same process from the other direction?
+	    // shouldn't be needed, hopefully..
+	    
+	    // shift for positive values
+	    float min = 1e9f;
+	    for (int xyz=0;xyz<nxyz;xyz++) if (mask[xyz])
+	        if (image[xyz]<min) min = image[xyz];
+	    for (int xyz=0;xyz<nxyz;xyz++) if (mask[xyz])
+	        image[xyz] -= min;
+
+	    regularised = image;
+	    
+		System.out.print("Done\n");
+	}
 }
